d4ac0728c58463a9e493266ad5505bc065df8465
unknown <Tawakalt10>
2023-10-18 09:14:23 +0100

    new changes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav");
 
 
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+// hamburger.addEventListener("click", () =>{
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
 
 
-document.querySelectorAll(".list-item").forEach(n => n.
-    addEventListener("click", () => {
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
-    }))
+// document.querySelectorAll(".list-item").forEach(n => n.
+//     addEventListener("click", () => {
+//         hamburger.classList.remove("active")
+//         navMenu.classList.remove("active")
+//     }))
